788d455ce7cff086fc7569fa78442e6d70353929
Wes Young <wesley.young@du.edu>
2020-10-07 21:56:17 -0600

    Program generates password but still needs tweaks.

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,78 @@
-//Global variables for user inputs
-var specialChar = confirm("Would you like special characters?");
-var upperCase = confirm("Would you like upper case letters?");
-var lowerCase = confirm("Would you like lower case letters?");
-var numberUse = confirm("Would you like to use numbers?");
-console.log(specialChar);
-console.log(upperCase);
-console.log(lowerCase);
-console.log(numberUse);
+//Character Arrays
+var specialCharArray = ["$", "(", "<", "@", "[", "^", "_", "{"];
+var numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var lowerCaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var upperCaseArray = [];
+
+for (var i = 0; i < lowerCaseArray.length; i++) {
+  upperCaseArray[i] = lowerCaseArray[i].toUpperCase();
+}
+var combinedArray = [];
+
+for (var i = 0; i < specialCharArray.length; i++) {
+  console.log(specialCharArray[i]);
+}
+
+//return value for password variable in writePassword()
+function generatePassword() {
+  //Global variables for user inputs
+  var passwordLength = prompt("Choose length between 8 to 128");
+  var specialChar = confirm("Would you like special characters?");
+  var upperCase = confirm("Would you like upper case letters?");
+  var lowerCase = confirm("Would you like lower case letters?");
+  var numberUse = confirm("Would you like to use numbers?");
+
+  if (specialChar) {
+    combinedArray = combinedArray.concat(specialCharArray);
+  }
+
+  if (upperCase) {
+    combinedArray = combinedArray.concat(upperCaseArray);
+  }
+
+  if (lowerCase) {
+    combinedArray = combinedArray.concat(lowerCaseArray);
+  }
+
+  if (numberUse) {
+    combinedArray = combinedArray.concat(numbersArray);
+  }
+  var userPassword = "";
+  for (var i = 0; i < passwordLength; i++) {
+    indexLocation = Math.floor(Math.random() * combinedArray.length);
+    tempValue = String(combinedArray[indexLocation]);
+    userPassword = userPassword + tempValue;
+  }
+  return userPassword;
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -15,7 +81,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
